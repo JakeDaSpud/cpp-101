af43ce69f9a4a7cdea3019fd2b3786a42665c9c3
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-01-24 15:39:21 +0000

    - Added multiply - Added divide

--- main.cpp
+++ main.cpp
@@ -4,12 +4,23 @@
 
 // function declaration (or function prototype)
 int add(int, int);  // a function must be declared before it is called.
+int multiply(int, int);
+float divide(int, int);
 
 
 int main() {
     std::cout << "sam101 - calling add function from main()" << std::endl;
-    int sum = add(2,2);
+    int sum = add(3,7);
     std::cout <<  "Sum of values = " << sum << std::endl;
+
+    std::cout << "sam101 - calling multiply function from main()" << std::endl;
+    int product = multiply(10,3);
+    std::cout <<  "Product of values = " << product << std::endl;
+
+    std::cout << "sam101 - calling divide function from main()" << std::endl;
+    float divProduct = divide(1,2);
+    std::cout <<  "Division of values = " << divProduct << std::endl;
+
     return 0;
 }
 
@@ -23,9 +34,16 @@ int add( int x, int y) {    // two int parameters, returns int
 // arguments and returns an int value.  Test your function
 // and output the value it returns.
 
+int multiply( int x, int y) {
+    return x * y;
+}
+
 //TODO
 // 2. Define a function divide(int,int) that takes two integer arguments
 // and returns the result of dividing the first by the second.
 // Consider what the return type should be.
 // Output your result. Try calling divide(3,2)
 
+float divide( int x, int y) {
+    return ((float) x / y);
+}
